9fd28ceac9c7d062db2e00ab5a752065adb8dc20
UwU-tao <hieungo1410@gmail.com>
2024-06-18 17:54:23 +0700

    add

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,7 +5,7 @@ import scipy.sparse as sp
 import pickle
 
 def get_ub(dataname, task):
-    with open(os.path.join('../data/{}', 'user_bundle_{}.txt'.format(dataname, task)), 'r') as f:
+    with open(os.path.join('../data/{}'.format(dataname), 'user_bundle_{}.txt'.format(task)), 'r') as f:
         u_b_pairs = list(map(lambda s: tuple(int(i) for i in s[:-1].split('\t')), f.readlines()))
 
     indice = np.array(u_b_pairs, dtype=np.int32)
