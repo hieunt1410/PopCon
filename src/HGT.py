2726b8b9e1bc557501d49ee18d25456ac711e38a
UwU-tao <hieungo1410@gmail.com>
2024-06-26 09:40:21 +0700

    add

--- src/HGT.py
+++ src/HGT.py
@@ -57,7 +57,7 @@ class HGT(nn.Module):
             }
         )
 
-        avg_ub_seq = {"Youshu": 70, "NetEase": 30, "iFashion": 5}
+        avg_ub_seq = {"Youshu": 70, "NetEase": 30, "iFashion": 5, 'ML1M': 40}
         LiT_ub_conf = copy(self.conf)
         LiT_ub_conf["n_layer"] = self.conf["n_trans_layer"]
         LiT_ub_conf["num_sequence"] = LiT_ub_conf["n_user"]
